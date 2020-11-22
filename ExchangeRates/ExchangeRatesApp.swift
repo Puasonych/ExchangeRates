67656ebee6cca76e87dc12a37f3aab252e15a491
Erik Basargin <basargin.erik@gmail.com>
2020-11-22 16:11:45 +0000

    Initial Commit

---
+++ ExchangeRates/ExchangeRatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeRatesApp.swift
+//  ExchangeRates
+//
+//  Created by Erik Basargin on 22/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct ExchangeRatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
